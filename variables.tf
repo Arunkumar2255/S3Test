3b2d5bbbd0bd715b8dd2bf569d3dc413ede5b8c7
Arunkumar2255 <148421914+Arunkumar2255@users.noreply.github.com>
2024-05-21 18:43:48 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+}
+
+variable "versioning_enabled" {
+  description = "Enable versioning for the S3 bucket"
+  type        = bool
+  default     = true
+}
+
+variable "force_destroy" {
+  description = "Allows the bucket to be deleted even if it contains objects"
+  type        = bool
+  default     = false
+}
+
+variable "tags" {
+  description = "A map of tags to add to resources"
+  type        = map(string)
+  default     = {
+    Environment = "production"
+    Project     = "example-project"
+  }
+}
+
+
