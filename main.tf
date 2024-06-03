18bd8dc2dd39039084e63de1dc213a020dfd27e9
Arunkumar2255 <148421914+Arunkumar2255@users.noreply.github.com>
2024-06-03 17:58:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,31 +4,40 @@ resource "aws_s3_bucket" "main" {
   tags          = merge({
     Name = var.bucket_name
   }, var.tags)
+}
 
-  versioning {
-    enabled = var.versioning_enabled
+resource "aws_s3_bucket_versioning" "main" {
+  bucket = aws_s3_bucket.main.bucket
+
+  versioning_configuration {
+    status = var.versioning_enabled ? "Enabled" : "Suspended"
   }
+}
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+resource "aws_s3_bucket_server_side_encryption_configuration" "main" {
+  bucket = aws_s3_bucket.main.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
+}
 
-  logging {
-    target_bucket = var.log_bucket_name
-    target_prefix = "log/"
-  }
+resource "aws_s3_bucket_logging" "main" {
+  bucket        = aws_s3_bucket.main.bucket
+  target_bucket = var.log_bucket_name
+  target_prefix = "log/"
+}
 
-  lifecycle_rule {
+resource "aws_s3_bucket_lifecycle_configuration" "main" {
+  bucket = aws_s3_bucket.main.bucket
+
+  rule {
     id      = "log"
     enabled = true
 
-    filter {
-      prefix = "log/"
-    }
+    prefix = "log/"
 
     expiration {
       days = 90
@@ -77,5 +86,3 @@ resource "aws_s3_bucket_policy" "main" {
   bucket = aws_s3_bucket.main.id
   policy = data.aws_iam_policy_document.s3_bucket_policy.json
 }
-
-
